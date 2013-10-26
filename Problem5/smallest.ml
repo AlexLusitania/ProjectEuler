6db4eaf7d9cfae256eeccc47556cd91cae2e888b
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2013-10-27 00:37:22 +0200

    Smallest multiple number resolved

--- Problem5/smallest.ml
+++ Problem5/smallest.ml
@@ -4,3 +4,17 @@ Version : 1.0
 Date : 25/10/2013
 Description : ProjectEuler - Problem 5 - Smallest multiple - 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
 *)
+
+(* Return true if the given number (n) is evenly divisible by all of the numbers from x to y *)
+let is_divisible n x y =
+	let rec local i = match i with
+	| i when i>y -> true
+	| _ -> if (n mod i = 0) then local (i+1) else false
+	in local x;;
+
+(* Give the smallest positive number that is evenly divisible by all of the numbers from x to y *)
+let smallest_multiple x y =
+	let rec local i =
+	if (is_divisible i x y) then i
+	else local (i+y)
+	in local y;;	
