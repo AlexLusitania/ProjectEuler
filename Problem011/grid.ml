b62217154a476a6c2a380cd1b77dbc1cb68cb46a
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2014-01-08 13:38:01 +0100

    First tests to create a 20x20 matrix from a given grid (.txt file)

--- Problem011/grid.ml
+++ Problem011/grid.ml
@@ -7,6 +7,15 @@ Description : In a given 20x20 grid, what is the greatest product of four adjace
 
 (* First step : reading the file given containing the 20x20 grid and make a matrix out of it *)
 let file = open_in "grid.txt";;
-let str = input_line file;;
+(*let str = input_line file;;
 let characters_read = input file str 30 2;;
-print_string str;;
+print_string str;;*)
+
+let createMatrix data =
+	let str = input_line file in
+	let length = String.length str in
+	let rec local acc i = match i with
+	(*| i when i>(length*length) -> acc*)
+	| i when i>= length -> acc
+	| _ -> (* if(input file str i 2 = 2) then *) local (List.append acc [(int_of_string (String.sub str i 2))]) (i+3) (* else failwith "matrix creation fail" *)
+	in local [] 0;;
