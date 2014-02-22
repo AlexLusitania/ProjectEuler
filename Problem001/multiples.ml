8f3750102721f45e495eb91d87bac7585b228da2
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2014-02-22 09:33:50 +0100

    Remove useless argument (sum)

--- Problem001/multiples.ml
+++ Problem001/multiples.ml
@@ -5,9 +5,9 @@ Date : 21/10/2013
 Description : ProjectEuler - Problem 2 - If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 *)
 
-let multiples3_5 =
-	let rec local sum i = match i with
-	| 1000 -> sum
-	| _ -> if (i mod 3 = 0) || (i mod 5 = 0) then local (sum+i) (i+1)
-		   else local sum (i+1)
-	in local 0 0;;
+let multiples3_5 n =
+	let rec local i = match i with
+	| i when i=n -> 0
+	| _ -> if (i mod 3 = 0) || (i mod 5 = 0) then i+(local (i+1))
+		   else local (i+1)
+	in local 0;;
