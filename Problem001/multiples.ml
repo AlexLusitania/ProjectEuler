8e456cc5f4670e4d4a8897fc1850e6ed37a4a5dd
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2014-04-25 15:29:47 +0200

    Updated multiples.ml

--- Problem001/multiples.ml
+++ Problem001/multiples.ml
@@ -1,13 +1,14 @@
 (*
-Author : Alexandre Gomes
-Version : 1.0
+Author : AlexLusitania
+Version : 2.0
 Date : 21/10/2013
-Description : ProjectEuler - Problem 2 - If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+Updated : 25/04/2014
+Description : ProjectEuler - Problem 1 - If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 *)
 
 let multiples3_5 n =
 	let rec local i = match i with
 	| i when i=n -> 0
-	| _ -> if (i mod 3 = 0) || (i mod 5 = 0) then i+(local (i+1))
-		   else local (i+1)
+	| _ 		 -> if (i mod 3 = 0) || (i mod 5 = 0) then i + (local (i+1))
+		   											  else local (i+1)
 	in local 0;;
